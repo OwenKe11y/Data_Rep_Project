107bb5e0620f57360718fa514247aa07dfae0871
OwenKe11y <g00366614@gmit.ie>
2020-12-29 20:59:41 +0000

    started first page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+//Imports
+import React, { Component } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Navbar, Nav } from 'react-bootstrap';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar fixed="top" className="nav-main" variant="dark">
+          <Navbar.Brand href="#home">HomeFit</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+
+        </Navbar>
+        <main className="main">
+          
+          <section className="section parallax parallax-one"></section>
+          <section className="section static static-one">Static</section>
+          <section className="section parallax parallax-two"></section>
+          <section className="section static static-two">Static</section>
+          
+        </main>
+        
+      </div>
+    );
+  }
 }
-
 export default App;
